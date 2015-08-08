076ffe4d1f80f8ef63b714317bcf054fd242ca97
Giovanni Fernandez-Kincade <giokincade@gmail.com>
2015-08-08 10:47:50 -0400

    job

---
+++ src/main/scala/com/giokincade/scalding/ExampleJob.scala
@@ -0,0 +1,25 @@
+package com.twitter.scalding.examples
+
+import com.twitter.scalding._
+import org.apache.hadoop.util.ToolRunner
+import org.apache.hadoop.conf.Configuration
+
+class WordCountJob(args: Args) extends Job(args) {
+  TypedPipe.from(TextLine(args("input")))
+    .flatMap { line => tokenize(line) }
+    .groupBy { word => word } // use each word for a key
+    .size // in each group, get the size
+    .write(TypedTsv[(String, Long)](args("output")))
+
+  // Split a piece of text into individual words.
+  def tokenize(text: String): Array[String] = {
+    // Lowercase each word and remove punctuation.
+    text.toLowerCase.replaceAll("[^a-zA-Z0-9\\s]", "").split("\\s+")
+  }
+}
+
+object WordCountJob {
+  def main(args: Array[String]) {
+    ToolRunner.run(new Configuration, new Tool, args);
+  }
+}
