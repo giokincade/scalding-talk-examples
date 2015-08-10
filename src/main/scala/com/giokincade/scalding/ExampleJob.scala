2abd16efe353bda7ff55acd83722248ccd4fd1f2
Giovanni Fernandez-Kincade <giokincade@gmail.com>
2015-08-10 09:30:30 -0400

    whitespace

--- src/main/scala/com/giokincade/scalding/ExampleJob.scala
+++ src/main/scala/com/giokincade/scalding/ExampleJob.scala
@@ -7,17 +7,17 @@ import com.twitter.scalding._
 
 class ExampleJob(args: Args) 
   extends Job(args) {
+
   TextLine("data/words.txt")
     .map('line -> 'first_character) {
       (line:String) =>
         line.substring(0,1)
-  }
-  .groupBy('first_character) {
-    (g: GroupBuilder) =>
-      g.size('size)
-  }
-  .write(Tsv("data/output/characters.tsv"))
-  
+    }
+    .groupBy('first_character) {
+      (g: GroupBuilder) =>
+        g.size('size)
+    }
+    .write(Tsv("data/output/characters.tsv"))
 }
 
 object ExampleJob {
